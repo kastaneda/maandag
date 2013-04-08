672eead4909ad675be477620ced13a9be0729537
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-04-08 18:09:03 +0300

    More examples

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -19,6 +19,9 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     public function testExampleCom() {
         $this->session->open('http://www.example.com/');
         $this->assertEquals('Example Domain', $this->session->title());
+        $this->assertContains('Example Domain', $this->session->element('xpath', '//h1')->text());
+        $this->session->element('partial link text', 'More')->click();
+        $this->assertEquals('http://www.iana.org/domains/special', $this->session->url());
     }
 
 }
