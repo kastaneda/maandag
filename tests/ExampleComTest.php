f8ed5026195b7a4e12da1d1df1b49cc7a49d1dc5
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-05-07 02:16:20 +0300

    Better structure

--- tests/ExampleComTest.php
+++ tests/ExampleComTest.php
@@ -8,11 +8,19 @@ class HelloWorldTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->setBrowserUrl('http://www.example.com/');
     }
 
-    public function testExampleCom() {
+    public function testTitle() {
         $this->url('http://www.example.com/');
         $this->assertEquals('Example Domain', $this->title());
+    }
+
+    public function testContent() {
+        $this->url('http://www.example.com/');
         $this->assertContains('Example Domain', $this->byXPath('//h1')->text());
         $this->assertContains('This domain', $this->byCssSelector('p')->text());
+    }
+
+    public function testLink() {
+        $this->url('http://www.example.com/');
         $this->byLinkText('More information...')->click();
         $this->assertEquals('http://www.iana.org/domains/special', $this->url());
     }
