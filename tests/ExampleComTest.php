a7c88707d735edded22931ea08445963a7f8d2ff
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-05-25 17:27:55 +0300

    Move to Instaclick's version of WebDriver

--- tests/ExampleComTest.php
+++ tests/ExampleComTest.php
@@ -4,35 +4,37 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
     protected static $webDriverHost = 'http://localhost:4444/wd/hub';
     protected static $webDriver;
-    protected static $session;
+    protected static $webDriverSession;
+    protected $session;
 
     public static function setUpBeforeClass() {
-        static::$webDriver = new PHPWebDriver_WebDriver(static::$webDriverHost);
-        static::$session = static::$webDriver->session('firefox');
+        static::$webDriver = new WebDriver\WebDriver(static::$webDriverHost);
+        static::$webDriverSession = static::$webDriver->session('firefox');
     }
 
     public static function tearDownAfterClass() {
-        static::$session->close();
+        static::$webDriverSession->close();
     }
 
     public function setUp()
     {
-        static::$session->open('http://www.example.com/');
+        $this->session = static::$webDriverSession;
+        $this->session->open('http://www.example.com/');
     }
 
     public function testTitle() {
-        $this->assertEquals('Example Domain', static::$session->title());
+        $this->assertEquals('Example Domain', $this->session->title());
     }
 
     public function testContent() {
-        $this->assertContains('Example Domain', static::$session->element('xpath', '//h1')->text());
-        $this->assertContains('This domain', static::$session->element('css selector', 'p')->text());
+        $this->assertContains('Example Domain', $this->session->element('xpath', '//h1')->text());
+        $this->assertContains('This domain', $this->session->element('css selector', 'p')->text());
     }
 
     public function testLink() {
-        $this->assertEmpty(static::$session->elements('partial link text', 'Something non-existent'));
-        static::$session->element('partial link text', 'More')->click();
-        $this->assertEquals('http://www.iana.org/domains/reserved', static::$session->url());
+        $this->assertEmpty($this->session->elements('partial link text', 'Something non-existent'));
+        $this->session->element('partial link text', 'More')->click();
+        $this->assertEquals('http://www.iana.org/domains/reserved', $this->session->url());
     }
 
 }
